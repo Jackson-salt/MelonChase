c145de36e0356a2f3917366a17ea825bc62bb8cd
Jackson <71522825+Jackson-eng@users.noreply.github.com>
2021-08-31 13:33:16 -0700

    Create Main.java

---
+++ src/me/jacksondasheng/melonChase/Main.java
@@ -0,0 +1,26 @@
+package me.jacksoneng.melonChase;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Server;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin
+{
+    public static Server server = Bukkit.getServer();
+    
+    @Override
+    public void onEnable()
+    {
+        server.getConsoleSender().sendMessage("Enabling MelonChase");
+    
+        server.getPluginManager().registerEvents(new MelonChase(), this);
+        
+        server.getConsoleSender().sendMessage("MelonChase enabled");
+    }
+    
+    @Override
+    public void onDisable()
+    {
+        server.getConsoleSender().sendMessage("MelonChase disabled");
+    }
+}
